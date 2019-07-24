6d472cf026c5619887b6e5928cd910d9b38b072b
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-24 14:00:41 -0700

    post route and test functional

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const uuid = require('uuid/v4');
 
 const bookmarks = require('../store');
 const logger = require('../logger');
@@ -19,61 +18,33 @@ bookmarkRouter
       .catch(err => next(err));
   })
 
-  .post(bodyParser, (req, res) => {
+  .post(bodyParser, (req, res, next) => {
     const { title, url, description, rating} = req.body;
-    // const db = req.app.get('db');
+    const db = req.app.get('db');
+    const newBookmark = {title, url, description, rating};
 
-    if(!title) {
-      logger.error('Title is required');
-      return res  
-        .status(400)
-        .send('Invalid data');
+    const requiredFields = ['title', 'url', 'rating'];
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).json({ message: `Field "${field}" is required`});
+      }
     }
-  
-    if(!url) {
-      logger.error('Url is required');
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  
-    if(!description) {
-      logger.error('Description is required');
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  
-    if(!rating) {
-      logger.error('Rating is required');
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  
-    //get an id
-    const id = uuid();
-  
-    const bookmark = {
-      title,
-      url,
-      description,
-      rating
-    };
-  
-    bookmarks.push(bookmark);
-    // BookmarkService.insertBookmark(db, )
-    logger.info(`Bookmark with id ${id} created`);
-  
-    res
-      .status(201)
-      .location(`http://localhost:8000/bookmark/${id}`)
-      .json(bookmark);
+
+    BookmarkService.insertBookmark(db, newBookmark)
+      .then(bookmark => {
+        
+        res
+          .status(201)
+          .location(`/bookmarks/${bookmark.id}`)
+          .json(bookmark);
+      })
+      .catch(next);
+    
   });
 
 bookmarkRouter
   .route('/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
     const { id } = req.params;
     const db = req.app.get('db');
 
@@ -83,39 +54,41 @@ bookmarkRouter
           logger.error(`Bookmark with id ${id} not found.`);
           return res
             .status(404)
-            .send('Bookmark not found');
+            .json({ error: { message: 'Bookmark not found'} });
         }
         res.json(bookmark);
-      });
+      })
+      .catch(err => next(err));
   })
 
-  .patch(bodyParser, (req, res) => {
-    const { title, url, description, rating } = req.body;
-    const {id} = req.params;
+// .patch(bodyParser, (req, res) => {
+//   const { title, url, description, rating } = req.body;
+//   const {id} = req.params;
   
-    const bookmark = bookmarks.find(bookmark => bookmark.id === id);
+//   const bookmark = bookmarks.find(bookmark => bookmark.id === id);
   
-    if(title) {
-      bookmark.title = title;
-    } else
-    if(url) {
-      bookmark.url = url;
-    } else
-    if (description) {
-      bookmark.description = description;
-    } else
-    if (rating) {
-      bookmark.rating = rating;
-    }
+//   if(title) {
+//     bookmark.title = title;
+//   } else
+//   if(url) {
+//     bookmark.url = url;
+//   } else
+//   if (description) {
+//     bookmark.description = description;
+//   } else
+//   if (rating) {
+//     bookmark.rating = rating;
+//   }
   
-    logger.info(`Bookmark with id ${id} was updated`);
+//   logger.info(`Bookmark with id ${id} was updated`);
   
-    res
-      .status(200)
-      .location(`http://localhost8000/bookmark/${id}`)
-      .json(bookmark);
+//   res
+//     .status(200)
+//     .location(`http://localhost8000/bookmark/${id}`)
+//     .json(bookmark);
   
-  })
+// })
+
   .delete((req, res) => {
     const { id } = req.params;
     const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id === id);
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -82,5 +82,48 @@ describe('Bookmarks Endpoints', () => {
     });
   });
   
+  describe('POST /bookmarks', () => {
+    it('creates a bookmark and responds with 201', () => {
+      const newBookmark = {
+        title: 'NatGeo',
+        url: 'http://asdf.com',
+        description: 'Our world is neat!',
+        rating: 5
+      };
 
+      return supertest(app)
+        .post('/bookmarks')
+        .set('Authorization', 'bearer ' + process.env.API_TOKEN)
+        .send(newBookmark)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(newBookmark.title);
+          expect(res.body.url).to.eql(newBookmark.url);
+          expect(res.body.description).to.eql(newBookmark.description);
+          expect(res.body.rating).to.eql(newBookmark.rating);
+          expect(res.body).to.have.property('id');
+          expect(res.headers.location).to.eql(`/bookmarks/${res.body.id}`);
+        })
+        .then(postRes => 
+          supertest(app)
+            .get(`/bookmarks/${postRes.body.id}`)
+            .set('Authorization', 'bearer ' + process.env.API_TOKEN)
+            .expect(postRes.body)
+        );
+    });
+
+    const requiredFields = ['title', 'url', 'rating'];
+    requiredFields.forEach(field => {
+      it(`responds with 400 "Field ${field} is required" if invalid data`, () => {
+        const invalidData = makeBookmarksArray()[0];
+        delete invalidData[field];
+
+        return supertest(app)
+          .post('/bookmarks')
+          .set('Authorization', 'bearer ' + process.env.API_TOKEN)
+          .send(invalidData)
+          .expect(400, { message: `Field "${field}" is required` });
+      });
+    });
+  });
 });
